cdca73d0e2fed35d8f04ae938354dc48a88f30f4
Jonathan Boyle <programmerfalcon@gmail.com>
2025-04-07 22:08:54 +0100

    chore: formatting

--- flake.nix
+++ flake.nix
@@ -1,29 +1,27 @@
 {
-	description = "A collection of templates from FalconProgrammer - Jonathan Boyle";
+  description = "A collection of templates from FalconProgrammer - Jonathan Boyle";
 
-	outputs = {self}: {
-		templates = {
+  outputs = {self}: {
+    templates = {
+      python-pdm = {
+        path = ./python-pdm;
+        description = "A template for a python project using pdm";
+      };
 
-			python-pdm = {
-				path = ./python-pdm;
-				description = "A template for a python project using pdm";
-			};
+      python-shell = {
+        path = ./python-shell;
+        description = "A template to create a python shell with certain packages";
+      };
 
-			python-shell = {
-				path = ./python-shell;
-				description = "A template to create a python shell with certain packages";
-			};
+      python-poetry2nix = {
+        path = ./python-poetry2nix;
+        description = "A template for a python project using poetry2nix";
+      };
 
-			python-poetry2nix = {
-				path = ./python-poetry2nix;
-				description = "A template for a python project using poetry2nix";
-			};
-
-			default-package-flake = {
-				path = ./default-package-flake;
-				description = "A basic flake for building a default.nix package";
-			};
-
-		};
-	};
+      default-package-flake = {
+        path = ./default-package-flake;
+        description = "A basic flake for building a default.nix package";
+      };
+    };
+  };
 }
