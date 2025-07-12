5d2d79e9a2e6d6700552856dd16ddce16790b74b
Jonathan Boyle <programmerfalcon@gmail.com>
2025-07-12 22:41:51 +0100

    feat: upgrade pythons to 312

--- python-poetry2nix/flake.nix
+++ python-poetry2nix/flake.nix
@@ -1,49 +1,46 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-		poetry2nix.url = "github:nix-community/poetry2nix";
-		poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-	outputs = {
-		self,
-		nixpkgs,
-		flake-utils,
-		poetry2nix,
-		...
-	}: let
-		supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-	in
-		flake-utils.lib.eachSystem supportedSystems (system: let
-				# import nixpkgs
-				pkgs =
-					import nixpkgs {
-						config.allowUnfree = true;
-						inherit system;
-					};
-				p2n = import poetry2nix {pkgs = pkgs;};
-				lib = pkgs.lib;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      # import nixpkgs
+      pkgs = import nixpkgs {
+        config.allowUnfree = true;
+        inherit system;
+      };
+      p2n = import poetry2nix {pkgs = pkgs;};
+      lib = pkgs.lib;
 
-				# python interpreter to use
-				python-interp = pkgs.python311;
+      # python interpreter to use
+      python-interp = pkgs.python312;
 
-				poetry-app =
-					p2n.mkPoetryApplication {
-						python = python-interp;
-						projectDir = ./.;
-						preferWheels = true;
-					};
-			in {
-				packages = {
-					myapp = poetry-app;
-					default = self.packages.${system}.myapp;
-				};
+      poetry-app = p2n.mkPoetryApplication {
+        python = python-interp;
+        projectDir = ./.;
+        preferWheels = true;
+      };
+    in {
+      packages = {
+        myapp = poetry-app;
+        default = self.packages.${system}.myapp;
+      };
 
-				devShells.default =
-					pkgs.mkShell {
-						inherit system;
-						buildInputs = with pkgs; [poetry] ++ [poetry-app.dependencyEnv];
-					};
-			});
+      devShells.default = pkgs.mkShell {
+        inherit system;
+        buildInputs = with pkgs; [poetry] ++ [poetry-app.dependencyEnv];
+      };
+    });
 }
--- python-shell/flake.nix
+++ python-shell/flake.nix
@@ -1,42 +1,39 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	outputs = {
-		self,
-		nixpkgs,
-		flake-utils,
-		...
-	}: let
-		supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-	in
-		flake-utils.lib.eachSystem supportedSystems (system: let
-				# import nixpkgs
-				pkgs =
-					import nixpkgs {
-						config.allowUnfree = true;
-						inherit system;
-					};
-				lib = pkgs.lib;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      # import nixpkgs
+      pkgs = import nixpkgs {
+        config.allowUnfree = true;
+        inherit system;
+      };
+      lib = pkgs.lib;
 
-				# python interpreter to use
-				python-interp = pkgs.python311;
+      # python interpreter to use
+      python-interp = pkgs.python312;
 
-				# python packages to use
-				python-with-pkgs =
-					python-interp.withPackages (ps:
-							with ps; [
-								typer
-								rich
-								tqdm
-							]);
-			in {
-				devShells.default =
-					pkgs.mkShell {
-						inherit system;
-						buildInputs = with pkgs; [python-with-pkgs];
-					};
-			});
+      # python packages to use
+      python-with-pkgs = python-interp.withPackages (ps:
+        with ps; [
+          typer
+          rich
+          tqdm
+        ]);
+    in {
+      devShells.default = pkgs.mkShell {
+        inherit system;
+        buildInputs = with pkgs; [python-with-pkgs];
+      };
+    });
 }
