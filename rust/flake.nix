23d878e76ab83264def9d18b63080a36b1c36585
Jonathan Boyle <programmerfalcon@gmail.com>
2026-01-24 12:47:42 +0000

    feat: rust full workflow

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,51 +1,67 @@
 {
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		flake-parts.url = "github:hercules-ci/flake-parts";
+		systems.url = "github:nix-systems/default";
+
+		treefmt-nix.url = "github:numtide/treefmt-nix";
+		nix-precommit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
 		fenix = {
 			url = "github:nix-community/fenix";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-		nix-precommit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+		crane = {
+			url = "github:ipetkov/crane";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = {
-		self,
-		nixpkgs,
-		flake-utils,
-		fenix,
-		nix-precommit-hooks,
-		...
-	}: let
-		supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-	in
-		flake-utils.lib.eachSystem supportedSystems (system: let
-				pkgs = import nixpkgs {
-					inherit system;
-					overlays = [fenix.overlays.default];
+	outputs = inputs:
+		inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+			systems = import inputs.systems;
+			imports = [
+				inputs.treefmt-nix.flakeModule
+				inputs.nix-precommit-hooks.flakeModule
+			];
+			perSystem = {
+				config,
+				self',
+				pkgs,
+				lib,
+				system,
+				...
+			}: let
+				cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+				fnx = inputs.fenix.packages.${system};
+
+				rust-toolchain = with fnx;
+					combine [
+						stable.rustc
+						stable.cargo
+						stable.rust-src
+						stable.rust-analyzer
+						stable.clippy
+						default.rustfmt
+					];
+
+				crn = (inputs.crane.mkLib pkgs).overrideToolchain rust-toolchain;
+
+				cargoArtifacts = crn.buildDepsOnly {
+					src = ./.;
+					pname = cargoToml.package.name;
+					strictDeps = true;
 				};
 
-				# Rust toolchain - latest stable with all components
-				rust-toolchain = pkgs.fenix.stable.withComponents [
-					"rustc"
-					"cargo"
-					"rust-src"
-					"rust-analyzer"
-					"clippy"
-					"rustfmt"
-				];
-
-				pre-commit-check =
-					nix-precommit-hooks.lib.${system}.run {
-						src = ./.;
-						hooks = {
-							rustfmt.enable = true;
-							clippy.enable = true;
-							alejandra.enable = true;
-						};
-					};
+				package = crn.buildPackage rec {
+					inherit (cargoToml.package) name version;
+					pname = cargoToml.package.name;
+					src = ./.;
+					inherit cargoArtifacts;
+					doCheck = false;
+				};
 
 				claude-post-commit-hook = pkgs.writeShellScriptBin "claude-post-commit-check" ''
 					file_path=$(${pkgs.jq}/bin/jq -r '.tool_input.file_path // empty')
@@ -53,7 +69,7 @@
 						exit 0
 					fi
 
-					# Only check Rust files (skip for non-Rust files)
+					# Only check Rust files
 					if [[ ! "$file_path" =~ \.rs$ ]]; then
 						exit 0
 					fi
@@ -67,37 +83,55 @@
 					fi
 				'';
 			in {
-				devShells.default = pkgs.mkShell {
-					inherit system;
-					shellHook = ''
-						${pre-commit-check.shellHook}
-						if [ -f .env ]; then
-							set -a
-							source .env
-							set +a
-						fi
-					'';
+				packages.default = package;
 
-					buildInputs = [
-						rust-toolchain
-						claude-post-commit-hook
-						pkgs.alejandra
-					];
+				pre-commit.settings = {
+					hooks = {
+						rustfmt = {
+							enable = true;
+							packageOverrides = {
+								rustfmt = fnx.default.rustfmt;
+								cargo = fnx.default.cargo;
+							};
+						};
+						alejandra.enable = true;
+					};
 				};
 
-				devShells.no-package = pkgs.mkShell {
-					inherit system;
-					shellHook = ''
-						if [ -f .env ]; then
-							set -a
-							source .env
-							set +a
-						fi
-					'';
+				devShells = {
+					default = crn.devShell {
+						inputsFrom = [
+							config.treefmt.build.devShell
+						];
+						shellHook = ''
+							${config.pre-commit.installationScript}
+							if [ -f .env ]; then
+								set -a
+								source .env
+								set +a
+							fi
+						'';
 
-					buildInputs = [
-						rust-toolchain
-					];
+						buildInputs = [
+							claude-post-commit-hook
+						];
+
+						RUST_BACKTRACE = 1;
+						RUST_SRC_PATH = "${fnx.stable.rust-src}/lib/rustlib/src/rust/library/";
+					};
+
+					no-package = pkgs.mkShell {
+						buildInputs = [rust-toolchain];
+					};
 				};
-			});
+
+				treefmt.config = {
+					projectRootFile = "flake.nix";
+					programs = {
+						alejandra.enable = true;
+						rustfmt.enable = true;
+					};
+				};
+			};
+		};
 }
